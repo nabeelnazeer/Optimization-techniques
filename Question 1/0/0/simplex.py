ac8f12f61a37739932846f558039a9e9d971463f
nabeelnazeer <nabeel777@ug.cusat.ac.in>
2024-04-07 01:04:34 +0530

    made tableau numpy array

--- Question 1/0/0/simplex.py
+++ Question 1/0/0/simplex.py
@@ -1,74 +1,67 @@
-
 import numpy as np
 
 def create_tableau(c, A, b):
     m, n = A.shape
-    tableau = np.zeros((m+1,m+n+1))
+    tableau = np.zeros((m + 1, m + n + 1))
     tableau[:-1, :-1] = A
     tableau[:-1, -1] = b
-    tableau[:-1, :n]
+    tableau[-1, :n] = c
+    return tableau
 
 def select_entering_variable(tableau):
     return np.argmax(tableau[-1, :-1])
 
 def select_leaving_variable(tableau, entering_variable):
     ratios = tableau[:-1, -1] / tableau[:-1, entering_variable]
+    return np.argmin(ratios)
 
 def pivot(tableau, key_row, key_col):
     m, n = tableau.shape
-    key_element = [key_row][key_col]
+    key_element = tableau[key_row, key_col]
     tableau[key_row, :] /= key_element
-
     for i in range(m):
         if i == key_row:
             continue
-        mulitiplier = tableau[i, key_col]
-        tableau[i, :] -= mulitiplier * tableau[key_row, : ]
-    return tableau      
+        multiplier = tableau[i, key_col]
+        tableau[i, :] -= multiplier * tableau[key_row, :]
+    return tableau
 
 def is_optimal(tableau):
-    return all(x<=0 for x in tableau[-1, :-1])
+    return all(x <= 0 for x in tableau[-1, :-1])
 
 def extract_solution(tableau):
     m, n = tableau.shape
-    solution = np.zeros(n-1)
-
-     
-
-
+    solution = np.zeros(n - 1)
+    basic_variables = np.argmax(tableau[:, :-1], axis=1)
+    for i, j in enumerate(basic_variables):
+        solution[j] = tableau[i, -1]
+    return solution
 
 def simplex(c, A, b):
-
     tableau = create_tableau(c, A, b)
-
     while not is_optimal(tableau):
-        key_col =  select_entering_variable(tableau)
+        key_col = select_entering_variable(tableau)
         key_row = select_leaving_variable(tableau, key_col)
-
-        key_element = tableau[key_row][key_col]
-
         tableau = pivot(tableau, key_row, key_col)
-
-    solution = extract_solution(tableau) 
-
+    solution = extract_solution(tableau)
     return solution
-   
+
 def main():
-   
     num_variables = int(input("Enter the number of variables: "))
     num_constraints = int(input("Enter the number of constraints: "))
 
-  
     c = [float(input(f"Enter coefficient for variable x{i+1} in the objective function: ")) for i in range(num_variables)]
 
-    A = []
-    print("Enter coefficients of constraint equations:")
-    for i in range(num_constraints):
-        coefficients = [float(input(f"Enter coefficient for variable x{j+1} in constraint equation {i+1}: ")) for j in range(num_variables)]
-        A.append(coefficients)
-    A = np.array(A)
+    # A = []
+    # print("Enter coefficients of constraint equations:")
+    # for i in range(num_constraints):
+    #     coefficients = [float(input(f"Enter coefficient for variable x{j+1} in constraint equation {i+1}: ")) for j in range(num_variables)]
+    #     A.append(coefficients)
+    # A = np.array(A)
 
-  
+    A = np.zeros((num_constraints,num_variables))
+    
+
     b = [float(input(f"Enter the right-hand side of constraint equation {i+1}: ")) for i in range(num_constraints)]
 
     solution = simplex(c, A, b)
